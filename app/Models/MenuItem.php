2c510dfd61895fd846b3f8f510a170e7cb969e21
Awais javed <awaisjaved025@gmail.com>
2022-06-09 04:26:21 -0400

    test completed: got menu items using recursive childs

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    public function subChild()
+    {
+        return $this->hasMany(MenuItem::class, 'parent_id');
+    }
 
+    public function children()
+    {
+        return $this->subChild()->with('children');
+    }
 }
